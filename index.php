a69fdfe3bf380d4d6f148025465d40a21bb61680
Gogo-58BG <gogo_58bg@yahoo.com>
2019-01-16 13:33:43 +0200

    new file:   index.php 	new file:   product.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$myName=$_GET ["myName"]." Petrov";
+$myAge=$_GET ["myAge"] + 40;
+?>
+<html>
+<p>
+test webapp
+My name is <?php echo $myName;?> And my age is <?php echo $myAge;?> 
+</p>
+</html>
